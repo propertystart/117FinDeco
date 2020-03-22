032e997bab4969f3ccdbf7b50be1d45296467555
PropertyStart Admin <info@propertystart.com.au>
2020-03-22 22:01:54 +1100

    Version 3

--- Build/UnityLoader.js
+++ Build/UnityLoader.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:09cf226e3d59b235f51dceec11eccd0cab4e415a132af33a2eaa3e5354deddd9
-size 157128
+oid sha256:3c1427188a1089905065b81e6bb0ab04f55ea50f17623e8828ca3e46a7ece73b
+size 159236
--- TemplateData/UnityProgress.js
+++ TemplateData/UnityProgress.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d0f54aef33f34fd123d1ebc048cf152e01294cb71809ce276e3e1304ba4e2830
-size 1186
+oid sha256:1e542a888b53f5e811ab967e08e95c629c18a3baa94d46d42fd859bd4dc13777
+size 1212
